fd4e634976a50a5f26d9b9628a3f0dcbbc43d3fd
jociel <jocielalvesdejesus@gmail.com>
2026-08-30 20:51:17 -0300

    cadastro js continua...

--- cadastro.js
+++ cadastro.js
@@ -10,6 +10,7 @@ document.getElementById("btn-cadastrar").addEventListener( 'click', () => {
         confirmaSenha: document.getElementById("confirma-senha").value
     };
 
+   const usuariosCadastrados = JSON.parse(localStorage.getItem(BANCO_USUARIO)) || [];
 
     if( novoUsuario.senha != novoUsuario.confirmaSenha){   
         const msgErro = document.getElementById("msg-erro");
@@ -18,9 +19,29 @@ document.getElementById("btn-cadastrar").addEventListener( 'click', () => {
 
         return;
     }
- 
-    localStorage.setItem(BANCO_USUARIO, JSON.stringify(novoUsuario));
-   
+
+    if( novoUsuario.nome === "" || novoUsuario.email === "" || novoUsuario.usuario === "" || novoUsuario.senha === "" ){
+        const msgErro = document.getElementById("msg-erro");
+        msgErro.textContent = "Preencha todos os campos...";
+        msgErro.style.color = "red"; 
+
+      return;
+   }  
+
+    if(usuariosCadastrados != null){
+        const usuarioExiste = usuariosCadastrados.some(element =>  element.email === novoUsuario.email || element.usuario === novoUsuario.usuario);
+        
+        if(usuarioExiste){
+        msgErro.textContent = "Email ou Usuário já utilizados";
+        msgErro.style.color = "red"; 
+            return;
+      }
+   }
+
+    usuariosCadastrados.push(novoUsuario);
+
+    localStorage.setItem(BANCO_USUARIO, JSON.stringify(usuariosCadastrados));
+    
     location.href = "login.html";
 
 });
