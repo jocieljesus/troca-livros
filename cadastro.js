e4f9d3797f34b259f03aaa11fc5f66a6c6a6c1b6
jocieljesus <jocielalvesdejesus@gmail.com>
2026-08-31 21:18:40 -0300

    atualiza cadastro

--- cadastro.js
+++ cadastro.js
@@ -1,6 +1,8 @@
 const BANCO_USUARIO = "usuariosCadastrados";
 
-document.getElementById("btn-cadastrar").addEventListener( 'click', () => {
+document.getElementById("form-caastro").addEventListener( 'submit', (event) => {
+
+    event.preventDefault();
 
     const novoUsuario = {
         nome: document.getElementById("nome").value,
@@ -10,24 +12,25 @@ document.getElementById("btn-cadastrar").addEventListener( 'click', () => {
         confirmaSenha: document.getElementById("confirma-senha").value
     };
 
-   const usuariosCadastrados = JSON.parse(localStorage.getItem(BANCO_USUARIO)) || [];
-
-    if( novoUsuario.senha != novoUsuario.confirmaSenha){   
-        const msgErro = document.getElementById("msg-erro");
+    const usuariosCadastrados = JSON.parse(localStorage.getItem(BANCO_USUARIO)) || [];
+    const msgErro = document.getElementById("msg-erro");
+    //Validação de senhas diferentes 
+    if( novoUsuario.senha != novoUsuario.confirmaSenha){  
         msgErro.textContent = "As senhas não coincidem. Tente novamente.";
         msgErro.style.color = "red"; 
 
         return;
     }
 
+      // validação que os campos estão preenchidos
     if( novoUsuario.nome === "" || novoUsuario.email === "" || novoUsuario.usuario === "" || novoUsuario.senha === "" ){
-        const msgErro = document.getElementById("msg-erro");
         msgErro.textContent = "Preencha todos os campos...";
         msgErro.style.color = "red"; 
 
       return;
    }  
 
+   //Validação se o usuário já existe
     if(usuariosCadastrados != null){
         const usuarioExiste = usuariosCadastrados.some(element =>  element.email === novoUsuario.email || element.usuario === novoUsuario.usuario);
         
@@ -41,6 +44,8 @@ document.getElementById("btn-cadastrar").addEventListener( 'click', () => {
     usuariosCadastrados.push(novoUsuario);
 
     localStorage.setItem(BANCO_USUARIO, JSON.stringify(usuariosCadastrados));
+
+    alert("Cadastro realizado com sucesso!");
     
     location.href = "login.html";
 
